91f5aeaa95dc303c941fd75c9b06d2bb3769a906
Page <pjgazzard@googlemail.com>
2013-03-27 17:49:57 +0000

    Add a test for 'or'

--- test/filterby.js
+++ test/filterby.js
@@ -79,6 +79,30 @@ test("/resource?$filterby=Price gt 5 and Price lt 10", "OData", function(result)
   })
 })
 
+test("/resource?$filterby=Price eq 5 or Price eq 10", "OData", function(result) {
+
+  it("A filter should be present", function() {
+     assert.notEqual(result.options.$filterby, null)
+  })
+  it("Filter should be an instance of 'or'", function() {
+     assert.equal(result.options.$filterby[0], "or")
+  })
+
+  it("Left hand side should be Price eq 5", function() {
+     var lhs = result.options.$filterby[1] 
+     assert.equal(lhs[0], "eq")
+     assert.equal(lhs[1].name, "Price")
+     assert.equal(lhs[2], 5)
+  })
+
+  it("Right hand side should be less than 10", function() {
+     var rhs = result.options.$filterby[2] 
+     assert.equal(rhs[0], "eq")
+     assert.equal(rhs[1].name, "Price")
+     assert.equal(rhs[2], 10)
+  })
+})
+
 test("/resource?$filterby=not Published", "OData", function(result) {
 
   it("A filter should be present", function() {
