d4d8cef45d32fc893c60d232fe5c57cfdff98c68
Page <pjgazzard@googlemail.com>
2013-03-28 00:11:59 +0000

    $filterby is actually $filter

--- test/filterby.js
+++ test/filterby.js
@@ -3,18 +3,18 @@ var assert = require('assert')
 
 
 function operandTest(op) {
-  test("/resource?$filterby=Foo " + op + " 2", "OData", function(result) {
+  test("/resource?$filter=Foo " + op + " 2", "OData", function(result) {
     it("A filter should be present", function() {
-       assert.notEqual(result.options.$filterby, null)
+       assert.notEqual(result.options.$filter, null)
     })
     it("Filter should be an instance of '" + op + "'", function() {
-       assert.equal(result.options.$filterby[0], op)
+       assert.equal(result.options.$filter[0], op)
     })
     it("lhr should be Foo", function() {
-       assert.equal(result.options.$filterby[1].name, "Foo")
+       assert.equal(result.options.$filter[1].name, "Foo")
     })
     it("rhr should be 2", function() {
-       assert.equal(result.options.$filterby[2], 2)
+       assert.equal(result.options.$filter[2], 2)
     })
   })
 }
@@ -25,160 +25,160 @@ operandTest("ge")
 operandTest("lt")
 operandTest("le")
 
-test("/resource?$filterby=Foo eq 'bar'", "OData", function(result) {
+test("/resource?$filter=Foo eq 'bar'", "OData", function(result) {
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'eq'", function() {
-     assert.equal(result.options.$filterby[0], "eq")
+     assert.equal(result.options.$filter[0], "eq")
   })
   it("lhr should be Foo", function() {
-     assert.equal(result.options.$filterby[1].name, "Foo")
+     assert.equal(result.options.$filter[1].name, "Foo")
   })
   it("rhr should be 2", function() {
-     assert.equal(result.options.$filterby[2], 'bar')
+     assert.equal(result.options.$filter[2], 'bar')
   })
 })
 
-test("/resource?$filterby=Foo eq 2.5", "OData", function(result) {
+test("/resource?$filter=Foo eq 2.5", "OData", function(result) {
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'eq'", function() {
-     assert.equal(result.options.$filterby[0], "eq")
+     assert.equal(result.options.$filter[0], "eq")
   })
   it("lhr should be Foo", function() {
-     assert.equal(result.options.$filterby[1].name, "Foo")
+     assert.equal(result.options.$filter[1].name, "Foo")
   })
   it("rhr should be 2.5", function() {
-     assert.equal(result.options.$filterby[2], 2.5)
+     assert.equal(result.options.$filter[2], 2.5)
   })
 })
 
-test("/resource?$filterby=Price gt 5 and Price lt 10", "OData", function(result) {
+test("/resource?$filter=Price gt 5 and Price lt 10", "OData", function(result) {
 
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'and'", function() {
-     assert.equal(result.options.$filterby[0], "and")
+     assert.equal(result.options.$filter[0], "and")
   })
 
   it("Left hand side should be Price gt 5", function() {
-     var lhs = result.options.$filterby[1] 
+     var lhs = result.options.$filter[1] 
      assert.equal(lhs[0], "gt")
      assert.equal(lhs[1].name, "Price")
      assert.equal(lhs[2], 5)
   })
 
   it("Right hand side should be less than 10", function() {
-     var rhs = result.options.$filterby[2] 
+     var rhs = result.options.$filter[2] 
      assert.equal(rhs[0], "lt")
      assert.equal(rhs[1].name, "Price")
      assert.equal(rhs[2], 10)
   })
 })
 
-test("/resource?$filterby=Price eq 5 or Price eq 10", "OData", function(result) {
+test("/resource?$filter=Price eq 5 or Price eq 10", "OData", function(result) {
 
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'or'", function() {
-     assert.equal(result.options.$filterby[0], "or")
+     assert.equal(result.options.$filter[0], "or")
   })
 
   it("Left hand side should be Price eq 5", function() {
-     var lhs = result.options.$filterby[1] 
+     var lhs = result.options.$filter[1] 
      assert.equal(lhs[0], "eq")
      assert.equal(lhs[1].name, "Price")
      assert.equal(lhs[2], 5)
   })
 
   it("Right hand side should be less than 10", function() {
-     var rhs = result.options.$filterby[2] 
+     var rhs = result.options.$filter[2] 
      assert.equal(rhs[0], "eq")
      assert.equal(rhs[1].name, "Price")
      assert.equal(rhs[2], 10)
   })
 })
 
-test("/resource?$filterby=not Published", "OData", function(result) {
+test("/resource?$filter=not Published", "OData", function(result) {
 
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'not'", function() {
-     assert.equal(result.options.$filterby[0], "not")
+     assert.equal(result.options.$filter[0], "not")
   })
 
   it("value should be 'Published'", function() {
-    assert.equal(result.options.$filterby[1].name, "Published")
+    assert.equal(result.options.$filter[1].name, "Published")
   })
 })
 
-test("/resource?$filterby=not (Price gt 5)", "OData", function(result) {
+test("/resource?$filter=not (Price gt 5)", "OData", function(result) {
 
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'not'", function() {
-     assert.equal(result.options.$filterby[0], "not")
+     assert.equal(result.options.$filter[0], "not")
   })
   it("Value should be Price gt 5", function() {
-     var rhs = result.options.$filterby[1] 
+     var rhs = result.options.$filter[1] 
      assert.equal(rhs[0], "gt")
      assert.equal(rhs[1].name, "Price")
      assert.equal(rhs[2], 5)
   })
 })
 
-test("/resource?$filterby=Price add 5 gt 10", "OData", function(result) {
+test("/resource?$filter=Price add 5 gt 10", "OData", function(result) {
 
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'gt'", function() {
-     assert.equal(result.options.$filterby[0], "gt")
+     assert.equal(result.options.$filter[0], "gt")
   })
   it("lhr should be Price add 5", function() {
-     var rhs = result.options.$filterby[1] 
+     var rhs = result.options.$filter[1] 
      assert.equal(rhs[0], "add")
      assert.equal(rhs[1].name, "Price")
      assert.equal(rhs[2], 5)
   })
   it("rhr should be 10", function() {
-     assert.equal(result.options.$filterby[2], 10)
+     assert.equal(result.options.$filter[2], 10)
   })
 })
 
-test("/resource?$filterby=Price mul 5 gt 10", "OData", function(result) {
+test("/resource?$filter=Price mul 5 gt 10", "OData", function(result) {
 
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'gt'", function() {
-     assert.equal(result.options.$filterby[0], "gt")
+     assert.equal(result.options.$filter[0], "gt")
   })
   it("lhr should be Price add 5", function() {
-     var lhs = result.options.$filterby[1] 
+     var lhs = result.options.$filter[1] 
      assert.equal(lhs[0], "mul")
      assert.equal(lhs[1].name, "Price")
      assert.equal(lhs[2], 5)
   })
   it("rhr should be 10", function() {
-     assert.equal(result.options.$filterby[2], 10)
+     assert.equal(result.options.$filter[2], 10)
   })
 })
 
-test("/resource?$filterby=Price div Price mul 5 gt 10", "OData", function(result) {
+test("/resource?$filter=Price div Price mul 5 gt 10", "OData", function(result) {
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'gt'", function() {
-     assert.equal(result.options.$filterby[0], "gt")
+     assert.equal(result.options.$filter[0], "gt")
   })
-  var lexpr = result.options.$filterby[1] 
+  var lexpr = result.options.$filter[1] 
 
   it("should be Price div {expr}", function() {
     assert.equal(lexpr[0], "div")
@@ -192,18 +192,18 @@ test("/resource?$filterby=Price div Price mul 5 gt 10", "OData", function(result
   })
 
   it("rhr should be 10", function() {
-     assert.equal(result.options.$filterby[2], 10)
+     assert.equal(result.options.$filter[2], 10)
   })
 })
 
-test("/resource?$filterby=(Price div Price) mul 5 gt 10", "OData", function(result) {
+test("/resource?$filter=(Price div Price) mul 5 gt 10", "OData", function(result) {
   it("A filter should be present", function() {
-     assert.notEqual(result.options.$filterby, null)
+     assert.notEqual(result.options.$filter, null)
   })
   it("Filter should be an instance of 'gt'", function() {
-     assert.equal(result.options.$filterby[0], "gt")
+     assert.equal(result.options.$filter[0], "gt")
   })
-  var lexpr = result.options.$filterby[1] 
+  var lexpr = result.options.$filter[1] 
 
   it("should be {expr} mul 5", function() {
     assert.equal(lexpr[0], "mul")
@@ -217,7 +217,7 @@ test("/resource?$filterby=(Price div Price) mul 5 gt 10", "OData", function(resu
   })
 
   it("rhr should be 10", function() {
-     assert.equal(result.options.$filterby[2], 10)
+     assert.equal(result.options.$filter[2], 10)
   })
 })
 
@@ -254,7 +254,7 @@ methodTestWithThreeArgs("indexof", true)
 */
 
 function methodTestWithThreeArgs(methodName, expectFailure) {
-  test("/resource?$filterby=" + methodName + "('alfred', Product, 2) eq 'cake'", "OData", function(result, err) {
+  test("/resource?$filter=" + methodName + "('alfred', Product, 2) eq 'cake'", "OData", function(result, err) {
     if(expectFailure)
       it("Should fail because it's invalid", function() {
         assert.notEqual(err, null)
@@ -262,28 +262,28 @@ function methodTestWithThreeArgs(methodName, expectFailure) {
     else if(err)
       throw err
     it("A filter should be present", function() {
-       assert.notEqual(result.options.$filterby, null)
+       assert.notEqual(result.options.$filter, null)
     })
     it("Filter should be an instance of 'eq'", function() {
-       assert.equal(result.options.$filterby[0], "eq")
+       assert.equal(result.options.$filter[0], "eq")
     })
     it("lhs should be a function call", function() {
-       assert.equal(result.options.$filterby[1][0], "call")
+       assert.equal(result.options.$filter[1][0], "call")
     })
     it("lhs should be " + methodName + " with correct args", function() {
-       assert.equal(result.options.$filterby[1][1].method, methodName)
-       assert.equal(result.options.$filterby[1][1].args[0], 'alfred')
-       assert.equal(result.options.$filterby[1][1].args[1].name, 'Product')
-       assert.equal(result.options.$filterby[1][1].args[2],2)
+       assert.equal(result.options.$filter[1][1].method, methodName)
+       assert.equal(result.options.$filter[1][1].args[0], 'alfred')
+       assert.equal(result.options.$filter[1][1].args[1].name, 'Product')
+       assert.equal(result.options.$filter[1][1].args[2],2)
     })
     it("rhs should be cake", function() {
-       assert.equal(result.options.$filterby[2], "cake")
+       assert.equal(result.options.$filter[2], "cake")
     }) 
   })
 }
 
 function methodTestWithTwoArgs(methodName, expectFailure) {
-  test("/resource?$filterby=" + methodName + "('alfred', Product) eq 'cake'", "OData", function(result, err) {
+  test("/resource?$filter=" + methodName + "('alfred', Product) eq 'cake'", "OData", function(result, err) {
     if(expectFailure)
       it("Should fail because it's invalid", function() {
         assert.notEqual(err, null)
@@ -291,27 +291,27 @@ function methodTestWithTwoArgs(methodName, expectFailure) {
     else if(err)
       throw err
     it("A filter should be present", function() {
-       assert.notEqual(result.options.$filterby, null)
+       assert.notEqual(result.options.$filter, null)
     })
     it("Filter should be an instance of 'eq'", function() {
-       assert.equal(result.options.$filterby[0], "eq")
+       assert.equal(result.options.$filter[0], "eq")
     })
     it("lhs should be a function call", function() {
-       assert.equal(result.options.$filterby[1][0], "call")
+       assert.equal(result.options.$filter[1][0], "call")
     })
     it("lhs should be " + methodName + " with correct args", function() {
-       assert.equal(result.options.$filterby[1][1].method, methodName)
-       assert.equal(result.options.$filterby[1][1].args[0], 'alfred')
-       assert.equal(result.options.$filterby[1][1].args[1].name, 'Product')
+       assert.equal(result.options.$filter[1][1].method, methodName)
+       assert.equal(result.options.$filter[1][1].args[0], 'alfred')
+       assert.equal(result.options.$filter[1][1].args[1].name, 'Product')
     })
     it("rhs should be cake", function() {
-       assert.equal(result.options.$filterby[2], "cake")
+       assert.equal(result.options.$filter[2], "cake")
     }) 
   })
 }
 
 function methodTestWithOneArg(methodName, expectFailure) {
-  test("/resource?$filterby=" + methodName + "('alfred') eq 'cake'", "OData", function(result, err) {
+  test("/resource?$filter=" + methodName + "('alfred') eq 'cake'", "OData", function(result, err) {
     if(expectFailure)
       it("Should fail because it's invalid", function() {
         assert.notEqual(err, null)
@@ -319,20 +319,20 @@ function methodTestWithOneArg(methodName, expectFailure) {
     else if(err)
       throw err
     it("A filter should be present", function() {
-       assert.notEqual(result.options.$filterby, null)
+       assert.notEqual(result.options.$filter, null)
     })
     it("Filter should be an instance of 'eq'", function() {
-       assert.equal(result.options.$filterby[0], "eq")
+       assert.equal(result.options.$filter[0], "eq")
     })
     it("lhs should be a function call", function() {
-       assert.equal(result.options.$filterby[1][0], "call")
+       assert.equal(result.options.$filter[1][0], "call")
     })
     it("lhs should be " + methodName + " with correct args", function() {
-       assert.equal(result.options.$filterby[1][1].method, methodName)
-       assert.equal(result.options.$filterby[1][1].args[0], 'alfred')
+       assert.equal(result.options.$filter[1][1].method, methodName)
+       assert.equal(result.options.$filter[1][1].args[0], 'alfred')
     })
     it("rhs should be cake", function() {
-       assert.equal(result.options.$filterby[2], "cake")
+       assert.equal(result.options.$filter[2], "cake")
     }) 
   })
 }
