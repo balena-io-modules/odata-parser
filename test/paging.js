f160a140209cc5c9d3abc617cf8de7a37a0abf16
Pagan Gazzard <page@balena.io>
2024-10-03 20:28:41 +0100

    Use bind vars for $top and $skip options
    Change-type: minor

--- test/paging.js
+++ test/paging.js
@@ -2,14 +2,21 @@ import * as assert from 'assert';
 
 export default (test) => {
 	describe('Paging', function () {
-		test('$top=5&$skip=100', function (result) {
-			it('top should be specified', () => {
-				assert.equal(result.options.$top, 5);
-			});
-			it('skip should be specified', () => {
-				assert.equal(result.options.$skip, 100);
-			});
-		});
+		test(
+			'$top=5&$skip=100',
+			[
+				['Integer', 5],
+				['Integer', 100],
+			],
+			function (result) {
+				it('top should be specified', () => {
+					assert.deepEqual(result.options.$top, { bind: 0 });
+				});
+				it('skip should be specified', () => {
+					assert.deepEqual(result.options.$skip, { bind: 1 });
+				});
+			},
+		);
 
 		test('$inlinecount=allpages', (result) => {
 			it('inline should be specified', () => {
